58a07e841c87f28d26065d037557ec0e0dec3a8b
Abu <qxes1125@gmail.com>
2026-08-20 08:50:19 +0800

    Commit: route fix

--- route.js
+++ route.js
@@ -1,58 +1,58 @@
 export const ROUTE = [
   {
     targetIndex: 0,
-    title: "Tibame 後門已定位",
-    instruction: "請往前走",
-    direction: "forward",
-  },
-
-  {
-    targetIndex: 1,
-    title: "已到路口",
-    instruction: "請往前走",
-    direction: "forward",
-  },
-
-  {
-    targetIndex: 2,
-    title: "已到大廳",
-    instruction: "請往右轉",
+    title: "Tibame 已定位",
+    instruction: "你快到Tibame了，找到正門口",
     direction: "right",
   },
 
   {
+    targetIndex: 1,
+    title: "已到正們口",
+    instruction: "往前到電梯口，搭乘電梯",
+    direction: "left",
+  },
+
+  {
+    targetIndex: 2,
+    title: "電梯口",
+    instruction: "請搭乘電梯到4樓",
+    direction: "forward",
+  },
+
+  {
     targetIndex: 3,
-    title: "已到電梯口",
-    instruction: "請往前走",
+    title: "4樓櫃台",
+    instruction: "已到4樓櫃台，前往走廊",
     direction: "forward",
   },
 
   {
     targetIndex: 4,
-    title: "已到走廊",
-    instruction: "往右轉",
+    title: "前往走廊",
+    instruction: "前往走廊，請往右轉",
     direction: "right",
   },
 
   {
     targetIndex: 5,
-    title: "走廊已定位",
+    title: "走廊",
     instruction: "請往左轉",
     direction: "left",
   },
 
   {
     targetIndex: 6,
-    title: "繼續前進",
-    instruction: "請往前走",
-    direction: "forward",
+    title: "走廊",
+    instruction: "繼續往前走，左轉",
+    direction: "left",
   },
 
   {
     targetIndex: 7,
-    title: "快到了",
-    instruction: "請往前走",
-    direction: "forward",
+    title: "走廊",
+    instruction: "繼續往前走，右轉",
+    direction: "right",
   },
 
   {
